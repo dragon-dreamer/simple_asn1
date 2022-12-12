613dbad760d6d9da23b9c6b4b98208cab520ebd6
Denis T <dragon-dreamer@users.noreply.github.com>
2022-12-12 19:45:13 +0000

    Added license headers to source files

--- simple_asn1/crypto/x509/spec.h
+++ simple_asn1/crypto/x509/spec.h
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: MIT
+
 #pragma once
 
 #include "simple_asn1/crypto/crypto_common_spec.h"
--- simple_asn1_tests/crypto.cpp
+++ simple_asn1_tests/crypto.cpp
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: MIT
+
 #include <array>
 #include <cstdint>
 #include <span>
