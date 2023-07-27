e9d282d97035ff59f42fd03a270b4cb99962ba84
Denis T <dragon-dreamer@users.noreply.github.com>
2023-07-27 22:26:06 +0100

    Add new OIDs

--- simple_asn1/include/simple_asn1/crypto/pkcs7/oids.h
+++ simple_asn1/include/simple_asn1/crypto/pkcs7/oids.h
@@ -18,4 +18,8 @@ constexpr std::array oid_content_type = std::to_array<std::uint32_t>({
 constexpr std::array oid_signing_time = std::to_array<std::uint32_t>({
 	1u, 2u, 840u, 113549u, 1u, 9u, 5u
 });
+
+constexpr std::array oid_signed_data = std::to_array<std::uint32_t>({
+	1u, 2u, 840u, 113549u, 1u, 7u, 2u
+});
 } //namespace asn1::crypto::pkcs7
--- simple_asn1/include/simple_asn1/crypto/pkcs9/oids.h
+++ simple_asn1/include/simple_asn1/crypto/pkcs9/oids.h
@@ -14,4 +14,9 @@ constexpr std::array oid_timestamp_token = std::to_array<std::uint32_t>({
 constexpr std::array oid_counter_signature = std::to_array<std::uint32_t>({
 	1u, 2u, 840u, 113549u, 1u, 9u, 6u
 });
+
+constexpr std::array oid_tst_info = std::to_array<std::uint32_t>({
+	1u, 2u, 840u, 113549u, 1u, 9u, 16u, 1u, 4u
+});
+
 } //namespace asn1::crypto::pkcs9
