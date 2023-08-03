e350e5397c61c46575a0724af3bd4bc7e1f8ee88
Denis T <dragon-dreamer@users.noreply.github.com>
2023-08-03 23:49:11 +0100

    Add more encryption algorithm OIDs

--- simple_asn1/include/simple_asn1/crypto/algorithms.h
+++ simple_asn1/include/simple_asn1/crypto/algorithms.h
@@ -33,6 +33,16 @@ constexpr auto id_ec_public_key = id_ansi_x9_62<2, 1>;
 constexpr auto id_dh_public_number = id_ansi_x9_42<2, 1>;
 constexpr auto id_rsa = std::to_array<std::uint32_t>({
 	1, 2, 840, 113549, 1, 1, 1 });
+constexpr auto id_sha256_with_rsa = std::to_array<std::uint32_t>({
+	1, 2, 840, 113549, 1, 1, 11 });
+constexpr auto id_md5_with_rsa = std::to_array<std::uint32_t>({
+	1, 2, 840, 113549, 1, 1, 4 });
+constexpr auto id_sha1_with_rsa = std::to_array<std::uint32_t>({
+	1, 2, 840, 113549, 1, 1, 5 });
+constexpr auto id_sha384_with_rsa = std::to_array<std::uint32_t>({
+	1, 2, 840, 113549, 1, 1, 12 });
+constexpr auto id_sha512_with_rsa = std::to_array<std::uint32_t>({
+	1, 2, 840, 113549, 1, 1, 13 });
 } //namespace pki
 
 namespace hash
